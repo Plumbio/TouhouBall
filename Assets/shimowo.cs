af357c6c9312c57fd49f3f3ccc66513a55c19cba
FrancoBeltranQuezada <francobeltranquezada@gmail.com>
2022-02-10 21:22:06 -0300

    Shimowo

---
+++ Assets/shimowo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shimowo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("owo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
