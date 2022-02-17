663e528ed1a63059b4c6b60a4f0a43ea8ed19f47
Plumbio <felipe.b@live.cl>
2022-02-17 01:15:11 -0300

    Código base de movimiento de pelota

---
+++ Assets/Scripts/Player/BallControllerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControllerScript : MonoBehaviour {
+
+    float speed = 5f;
+    Vector3 ballDirection;
+    bool moving;
+
+    void Start() {
+        //test code
+        ballDirection = new Vector3(1, 0, 0.6f).normalized;
+        moving = true;
+        //test code
+    }
+
+    void Update() {
+        if (moving)
+            StartCoroutine("Displace");
+    }
+
+    public void Hit (Vector3 direction) {
+        ballDirection = direction;
+    }
+
+    IEnumerator Displace () {
+        yield return new WaitForFixedUpdate();
+        transform.Translate(ballDirection * speed * Time.deltaTime);
+    }
+}
